5a41aee0ede8d842341a844812f31ed9225f17be
Samsung <bitup.games@gmail.com>
2023-05-25 12:15:33 +0200

    add: checkBalance before send

--- tasks/bridge.js
+++ tasks/bridge.js
@@ -5,21 +5,33 @@ module.exports = async function (taskArgs, hre) {
   const signers = await hre.ethers.getSigners()
   const owner = signers[0]
   const dstChainId = CHAIN_IDS[taskArgs.targetNetwork]
-  const amount = hre.ethers.utils.parseEther(taskArgs.amount)
+  
   const nativeOft = await hre.ethers.getContract(NATIVE_OFT_ARGS[hre.network.name].contractName)
   const swappableBridge = await hre.ethers.getContract("SwappableBridge")
-  const nativeFee = (await nativeOft.estimateSendFee(dstChainId, owner.address, amount, false, "0x")).nativeFee
-  const increasedNativeFee = nativeFee.mul(5).div(4)
+    
+  const amount = hre.ethers.utils.parseEther(taskArgs.amount)
+  try {
+    const nativeFee = (await nativeOft.estimateSendFee(dstChainId, owner.address, amount, false, "0x")).nativeFee
+    const total = nativeFee.mul(5).div(4).add(amount)
 
-  const tx = await swappableBridge.bridge(
-    amount,
-    dstChainId,
-    owner.address,
-    owner.address,
-    hre.ethers.constants.AddressZero,
-    "0x",
-    { value: amount.add(increasedNativeFee) }
-  )
-  await tx.wait()
-  console.log(`Transaction tx: ${tx.hash}\n`)
+    const balance = await hre.ethers.provider.getBalance(owner.address)
+
+    if (!balance.gt(total)) {
+      throw new Error(`Insuficient balance.`)
+    }
+
+    const tx = await swappableBridge.bridge(
+      amount,
+      dstChainId,
+      owner.address,
+      owner.address,
+      hre.ethers.constants.AddressZero,
+      "0x",
+      { value: amount.add(increasedNativeFee) }
+    )
+    await tx.wait()
+    console.log(`Transaction tx: ${tx.hash}\n`)
+  } catch (error) {
+    console.log('error on bridge', error);
+  }
 }
--- tasks/send.js
+++ tasks/send.js
@@ -4,29 +4,37 @@ module.exports = async function (taskArgs, hre) {
   let signers = await hre.ethers.getSigners()
   let owner = signers[0]
   let amount = hre.ethers.utils.parseEther(taskArgs.amount)
-  // get remote chain id
   const remoteChainId = CHAIN_IDS[taskArgs.targetNetwork]
-  // get local contract
   const localContract = await hre.ethers.getContract(taskArgs.contract)
 
-  // quote fee with default adapterParams
-  let adapterParams = hre.ethers.utils.solidityPack(["uint16", "uint256"], [1, 200000]) // default adapterParams example
+  try {
+    let adapterParams = hre.ethers.utils.solidityPack(["uint16", "uint256"], [1, 200000]) // default adapterParams example
+    let fees = await localContract.estimateSendFee(remoteChainId, owner.address, amount, false, adapterParams)
+    const increasedFee = fees[0].mul(10).div(9)
+    const balance = await hre.ethers.provider.getBalance(owner.address)
 
-  let fees = await localContract.estimateSendFee(remoteChainId, owner.address, amount, false, adapterParams)
-  console.log(`fees[0] (wei): ${fees[0]} / (eth): ${hre.ethers.utils.formatEther(fees[0])}`)
-  let tx = await (
-    await localContract.sendFrom(
-      owner.address,                 // 'from' address to send tokens
-      remoteChainId,                 // remote LayerZero chainId
-      owner.address,                     // 'to' address to send tokens
-      amount,
-      owner.address,
-      hre.ethers.constants.AddressZero,
-      "0x",
-      { value: fees[0].mul(10).div(9) }
-    )
-  ).wait()
-  console.log(`✅ Message Sent [${hre.network.name}] sendTokens() to OFT @ LZ chainId[${remoteChainId}] token:[${owner.address}]`)
-  console.log(` tx: ${tx.transactionHash}`)
-  console.log(`* check your address [${owner.address}] on the destination chain, in the ERC20 transaction tab !"`)
+    if (!balance.gt(increasedFee)) {
+      throw new Error(`Insuficient balance.`)
+    }
+
+    console.log(`Fee: ${hre.ethers.utils.formatEther(increasedFee)} ${hre.network.config.symbol}`);
+    let tx = await (
+      await localContract.sendFrom(
+        owner.address,                 // 'from' address to send tokens
+        remoteChainId,                 // remote LayerZero chainId
+        owner.address,                     // 'to' address to send tokens
+        amount,
+        owner.address,
+        hre.ethers.constants.AddressZero,
+        "0x",
+        { value: increasedFee }
+      )
+    ).wait()
+    console.log(`✅ Message Sent [${hre.network.name}] sendTokens() to OFT @ LZ chainId[${remoteChainId}] token:[${owner.address}]`)
+    console.log(` tx: ${tx.transactionHash}`)
+    console.log(`* check your address [${owner.address}] on the destination chain, in the ERC20 transaction tab !"`)
+  } catch (error) {
+    console.log('Error on send task', error);
+  }
+
 }
