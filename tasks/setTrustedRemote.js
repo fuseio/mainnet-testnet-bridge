5d79e6b090b560f94c2baaafaabb0aec188bd0f7
Samsung <dimension.inf24@hotmail.com>
2023-06-27 15:00:52 +0200

    update: tasks scripts

--- tasks/setTrustedRemote.js
+++ tasks/setTrustedRemote.js
@@ -4,23 +4,20 @@ const OFT_ARGS = require("../constants/oftArgs.json")
 const NATIVE_OFT_ARGS = require("../constants/nativeOftArgs.json")
 
 module.exports = async function (taskArgs, hre) {
-    const localChain = hre.network.name;
-    const remoteChain = taskArgs.targetNetwork;
+  const localChain = hre.network.name;
+  const remoteChain = taskArgs.targetNetwork;
+  const remoteChainId = CHAIN_IDS[remoteChain]
+  const localNativeOft = await ethers.getContract(NATIVE_OFT_ARGS[localChain].contractName)
+  const remoteNativeOft = getDeploymentAddresses(remoteChain)[NATIVE_OFT_ARGS[remoteChain].contractName]
 
-    const remoteChainId = CHAIN_IDS[remoteChain]
+  const localOft = await ethers.getContract(OFT_ARGS[localChain][remoteChain].contractName)
+  const remoteOft = getDeploymentAddresses(remoteChain)[OFT_ARGS[remoteChain][localChain].contractName]
 
-    const localNativeOft = await ethers.getContract(NATIVE_OFT_ARGS[localChain].contractName)
-    const remoteNativeOft = getDeploymentAddresses(remoteChain)[NATIVE_OFT_ARGS[remoteChain].contractName]
+  let tx = await localNativeOft.setTrustedRemoteAddress(remoteChainId, remoteOft)
+  console.log(`native OFT setTrustedRemoteAddress tx: ${tx.hash}`)
+  await tx.wait()
 
-    const localOft = await ethers.getContract(OFT_ARGS[localChain].contractName)
-    const remoteOft = getDeploymentAddresses(remoteChain)[OFT_ARGS[remoteChain].contractName]
-
-
-    let tx = await localNativeOft.setTrustedRemoteAddress(remoteChainId, remoteOft)
-    console.log(`native OFT setTrustedRemoteAddress tx: ${tx.hash}`)
-    await tx.wait()
-
-    tx = await localOft.setTrustedRemoteAddress(remoteChainId, remoteNativeOft)
-    console.log(`OFT setTrustedRemoteAddress tx: ${tx.hash}`)
-    await tx.wait()
+  tx = await localOft.setTrustedRemoteAddress(remoteChainId, remoteNativeOft)
+  console.log(`OFT setTrustedRemoteAddress tx: ${tx.hash}`)
+  await tx.wait()
 }
