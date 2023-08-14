4569a2cdff25a7fa3c6a7b820bcf4051e6931358
en0c-026 <bitup.games@gmail.com>
2023-08-14 14:47:33 +0200

    fix: info task

--- tasks/info.js
+++ tasks/info.js
@@ -8,18 +8,18 @@ const NATIVE_OFT_ARGS = require("../constants/nativeOftArgs.json")
 module.exports = async function (taskArgs, hre) {
   const signers = await ethers.getSigners()
   const owner = signers[0]
-  // const poolAddress = POOLS[hre.network.name]
-  // const weth = new ethers.Contract(WETHS[hre.network.name], ERC20Json.abi, owner)
-  const oft = await ethers.getContract(OFT_ARGS[hre.network.name].contractName)
+  const poolAddress = POOLS[hre.network.name][taskArgs.targetNetwork]
+  const weth = new ethers.Contract(WETHS[hre.network.name], ERC20Json.abi, owner)
+  const oft = await ethers.getContract(OFT_ARGS[hre.network.name][taskArgs.targetNetwork].contractName)
   const nativeOft = await ethers.getContract(NATIVE_OFT_ARGS[hre.network.name].contractName)
 
   console.log(`Account: ${owner.address}`);
   console.log(`${hre.network.config.symbol} balance: ${ethers.utils.formatEther(await hre.ethers.provider.getBalance(owner.address))}`)
   console.log(`${(await nativeOft.name())} balance: ${ethers.utils.formatEther(await nativeOft.balanceOf(owner.address))}`)
   console.log(`${(await oft.name())} balance: ${ethers.utils.formatEther(await oft.balanceOf(owner.address))}`)
+  console.log('-----------------------------');
+  console.log(`Pool: ${poolAddress}`);
+  console.log(`${(await weth.name())} balance: ${ethers.utils.formatEther(await weth.balanceOf(poolAddress))}`)
+  console.log(`${(await oft.name())} balance: ${ethers.utils.formatEther(await oft.balanceOf(poolAddress))}`)
 
-  // console.log(`ETH owner balance: ${ethers.utils.formatEther(await ethers.provider.getBalance(owner.address))}`)
-
-  // console.log(`OFT pool balance:  ${ethers.utils.formatEther(await oft.balanceOf(poolAddress))}`);
-  // console.log(`ETH pool balance:  ${ethers.utils.formatEther(await weth.balanceOf(poolAddress))}`);
 }
